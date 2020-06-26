97a1d7e2589d1c674a59c30ef5855c7ca7c544b2
alew <ale.w.es@sync.money>
2020-06-26 18:09:11 +0200

    update migration to mint 10 tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,24 @@
 var SquareVerifier = artifacts.require("./SquareVerifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier).then(() => {
-    return deployer.deploy(SolnSquareVerifier, SquareVerifier.address, "Udacity AW Token", "UAWT");
-  });
+const proofs = require("../zokrates/proofs.json");
+
+const tokenName = "Udacity AW Token";
+const tokenSymbol = "UAWT";
+
+const tokenBaseUrl = "UAWT/";
+
+module.exports = async function(deployer, network, accounts) {
+  const owner = accounts[0];
+  const tokenHolder = accounts[1];
+
+  await deployer.deploy(SquareVerifier);
+  await deployer.deploy(SolnSquareVerifier, SquareVerifier.address, tokenName, tokenSymbol);
+
+  const instance = await SolnSquareVerifier.deployed();
+  proofs.forEach(async (data, i) => {
+    const { proof: { a, b, c, }, inputs } = data;
+    const url = `${tokenBaseUrl}${i}`;
+    await instance.mintToken(tokenHolder, i, url, a, b, c, inputs, { from: owner });
+  })
 };
